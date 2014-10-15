412cb97d33bf11ed5822c29727a6db506b5b70ce
Jan Nelson <jan@learnist.com>
2014-10-15 01:58:22 -0700

    move log-layout configuration out of learnist rails app into logtool

--- lib/logtool.rb
+++ lib/logtool.rb
@@ -3,3 +3,9 @@ require 'fileutils'
 Dir["#{File.dirname(__FILE__)}/**/*.rb"].each do |filename|
   require filename unless filename == __FILE__
 end
+
+module Logtool
+  # Applications should use this layout for log messages to ensure that logtool
+  # will later be able to parse the necessary information back out of the logs.
+  LAYOUT = Logging.layouts.pattern(pattern: '[%d] [%p] %l <%c> - %m\n', date_pattern: "%Y-%m-%dT%H:%M:%S")
+end
