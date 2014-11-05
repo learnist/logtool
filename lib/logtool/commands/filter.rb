92ab3ca680a2584bba479ee8b48611062d1c8a93
Jan Nelson <jan@learnist.com>
2014-11-05 13:12:03 -0800

    add 'logtool filter' command

---
+++ lib/logtool/commands/filter.rb
@@ -0,0 +1,37 @@
+module Logtool
+  module Command
+    class Filter
+
+      def run(args)
+        inclusions, exclusions = [], []
+        while args.any? && args.first =~ /^--/
+          case args.shift
+            when '--include'; inclusions << args.shift
+            when '--exclude'; exclusions << args.shift
+            else exit_with_usage
+          end
+        end
+
+        if inclusions.empty? && exclusions.empty?
+          exit_with_usage
+        end
+
+        sources = args.map{|arg| Logtool::Source.new(arg) }
+        Logtool::RailsCollator.new(sources).run do |buffer|
+          endpoint = buffer.lines[1][/Processing by (\S+)/, 1]
+          raise "oops" if endpoint.nil?
+
+          if (inclusions.empty? || inclusions.include?(endpoint)) && !exclusions.include?(endpoint)
+            puts buffer
+            puts
+          end
+        end
+      end
+
+      def exit_with_usage
+        $stderr.puts "usage: logtool filter [ --include <endpoint> | --exclude <endpoint>] ..."
+        exit 1
+      end
+    end
+  end
+end
